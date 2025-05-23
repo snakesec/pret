cef24b203980ce8329d88f148c20048f1db6b93c
Weidsom Nascimento <source@snakesecurity.org>
2025-05-23 12:19:46 -0700

    Update pret source code to HARDBASED 5.0

---
+++ AFOSBUILD.sh
@@ -0,0 +1,30 @@
+rm -rf /opt/ANDRAX/pret
+
+python3 -m venv /opt/ANDRAX/pret
+
+source /opt/ANDRAX/pret/bin/activate
+
+/opt/ANDRAX/pret/bin/pip3 install requests colorama pysnmp
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Pip install... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+cp -Rf $(pwd) /opt/ANDRAX/pret/package
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+cp -Rf andraxbin/* /opt/ANDRAX/bin
+rm -rf andraxbin
